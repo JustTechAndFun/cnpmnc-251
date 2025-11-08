7f14b6903d5837f435c2f23f9e15ffc8a1f3931f
layducky <tranphuocnhatpnt@gmail.com>
2025-11-08 11:30:24 +0700

    feat: take & submit test FE

--- frontend/src/routes/student.routes.tsx
+++ frontend/src/routes/student.routes.tsx
@@ -5,6 +5,7 @@ import { StudentLayout } from '../components/StudentLayout';
 import { StudentDashboard } from '../pages/student/StudentDashboard';
 import { MyCourses } from '../pages/student/MyCourses';
 import { MyAssignments } from '../pages/student/MyAssignments';
+import { MyExams } from '../pages/student/MyExams';
 import { MyGrades } from '../pages/student/MyGrades';
 import { ProfilePage } from '../pages/ProfilePage';
 
@@ -28,6 +29,10 @@ export const studentRoutes: RouteObject[] = [
             {
                 path: 'assignments',
                 element: <MyAssignments />
+            },
+            {
+                path: 'exams/:examId',
+                element: <MyExams />
             },
             {
                 path: 'grades',
